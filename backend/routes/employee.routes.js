d09383bb863ec2aa5ac3780caea3cadeaf5564ec
Robel-dejene <robeldejene33@gmail.com>
2024-07-31 11:07:10 +0300

    using the auth.middleware

--- backend/routes/employee.routes.js
+++ backend/routes/employee.routes.js
@@ -9,31 +9,31 @@ const authMiddleware = require("../middlewares/auth.middleware");
 // Create a route to handle the add new employee request on post
 router.post(
   "/api/employee",
-  // [authMiddleware.verifyToken, authMiddleware.isAdmin],
+  [authMiddleware.verifyToken, authMiddleware.isAdmin],
   employeeController.createEmployee
 );
 // Create a route to handle the get All employees request on get
 router.get(
   "/api/employees",
-  // [authMiddleware.verifyToken, authMiddleware.isAdmin],
+  [authMiddleware.verifyToken, authMiddleware.isAdmin],
   employeeController.getAllEmployees
 );
 // Create a route to handle the get single employee request on get
 router.get(
   "/api/employee/:id",
-  // [authMiddleware.verifyToken, authMiddleware.isAdmin],
+  [authMiddleware.verifyToken, authMiddleware.isAdmin],
   employeeController.getEmployeeById
 );
 // Create a route to handle the update employee request on put
 router.put(
   "/api/employee",
-  // [authMiddleware.verifyToken, authMiddleware.isAdmin],
+  [authMiddleware.verifyToken, authMiddleware.isAdmin],
   employeeController.updateEmployee
 )
 // Create a route to handle the delete employee request on delete
 router.delete(
   "/api/employee/:id",
-  // [authMiddleware.verifyToken, authMiddleware.isAdmin],
+  [authMiddleware.verifyToken, authMiddleware.isAdmin],
   employeeController.deleteEmployee
 )
 // Export the router
