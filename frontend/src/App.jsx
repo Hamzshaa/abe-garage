d45f2bcf05894f1b73e9fbbb0d1d85feeb9138e1
Ayelechyilma <ayelech63@gmail.com>
2024-08-13 09:20:06 -0700

    add employeeList and Updat page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,11 +11,13 @@ import Header from "./markup/components/Header/Header";
 import Footer from "./markup/components/Footer/Footer";
 import AddEmployee from "./markup/pages/Admin/AddEmployee";
 import Unauthorized from "./markup/pages/Unauthorized";
+import DataTable from "./markup/components/Admin/EmployeesList/DataTable";
 
 import PrivateAuthRoute from "./markup/components/Auth/PrivateAuthRoute";
 import Home from "./markup/pages/Home";
 import Employees from "./markup/pages/Admin/Employees";
 import Services from "./markup/pages/Services";
+import UpdateEmployees from "./markup/pages/Admin/UpdateEmployees";
 
 function App() {
 	return (
@@ -23,6 +25,7 @@ function App() {
 			<Header />
 			<Routes>
 				<Route path="/" element={<Home />} />
+				{/* <Route path="/data" element={<DataTable />} /> */}
 				<Route path="/login" element={<Login />} />
 				<Route path="/services" element={<Services />} />
 				<Route path="/unauthorized" element={<Unauthorized />} />
@@ -34,7 +37,16 @@ function App() {
 						</PrivateAuthRoute>
 					}
 				/>
+
 				<Route path="/admin/employees" element={<Employees />} />
+				<Route
+					path="/admin/employee/edit/:id"
+					element={
+						<PrivateAuthRoute roles={[3]}>
+							<UpdateEmployees />
+						</PrivateAuthRoute>
+					}
+				/>
 			</Routes>
 			<Footer />
 		</>
